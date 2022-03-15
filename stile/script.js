d7617f4e8a73da61f1b45f07b045d3b2757fa72b
sidra1991 <1991sidra@gmail.com>
2022-03-15 18:46:03 +0100

    ci sono tutti

---
+++ stile/script.js
@@ -0,0 +1,126 @@
+
+
+const app = new Vue({
+    el: '#webApp',
+    data:{
+        boolzappList:[
+            {
+                id: 0,
+                user: 'io',
+                avatar: "img/avatar_io.jpg",
+                uTime: "12:00",
+                message: [
+                    {
+                        content:"hai portato giu il cane?",
+                        hours:"15:30",
+                    },
+                    {
+                        content:"ricordati di stendere i panni",
+                        hours:"15:50"
+                    },
+                ],
+            },
+            {
+                id: 1,
+                user: "Michele",
+                avatar: "img/avatar_1.jpg",
+                uTime: "12:00",
+                message: [
+                    {
+                        content:"si tutto fatto",
+                        hours:"16:15",
+                    },
+                ],    
+            },
+            {
+                id: 2,
+                user: "fabio",
+                avatar: `img/avatar_2.jpg`,
+                uTime: "12:00",
+                message: [
+                    {
+                        content:"",
+                        hours:"",
+                    },
+                ],    
+            },
+            {
+                id: 3,
+                user: "samuele",
+                avatar: `img/avatar_3.jpg`,
+                uTime: "12:00",
+                message: [
+                    {
+                        content:"",
+                        hours:"",
+                    },
+                ],    
+            },
+            {
+                id: 4,
+                user: "alessandro B.",
+                avatar: `img/avatar_4.jpg`,
+                uTime: "12:00",
+                message: [
+                    {
+                        content:"",
+                        hours:"",
+                    },
+                ],    
+            },
+            {
+                id: 5,
+                user: "Alessandro L",
+                avatar: `img/avatar_5.jpg`,
+                uTime: "12:00",
+                message: [
+                    {
+                        content:"",
+                        hours:"",
+                    },
+                ],    
+            },
+            {
+                id: 6,
+                user: "claudia",
+                avatar: `img/avatar_6.jpg`,
+                uTime: "12:00",
+                message: [
+                    {
+                        content:"",
+                        hours:"",
+                    },
+                ],    
+            },
+            {
+                id: 7,
+                user: "Federico",
+                avatar: `img/avatar_7.jpg`,
+                uTime: "12:00",
+                message: [
+                    {
+                        content:"",
+                        hours:"",
+                    },
+                ],    
+            },
+            {
+                id: 8,
+                user: "Davide",
+                avatar: `img/avatar_8.jpg`,
+                uTime: "12:00",
+                message: [
+                    {
+                        content:"",
+                        hours:"",
+                    },
+                ],    
+            },
+        ],
+    },
+    methods:{
+        
+
+
+    },
+});
